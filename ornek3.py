bf1ee1100137bc38c94edaec2ae43c5081f738a2
omurcetinim <omurcetin963@gmail.com>
2025-02-10 12:03:10 +0300

    Add files via upload

--- ornek3.py
+++
@@ -1,3 +0,0 @@
-gunler=["1. Gün Pazartesi","2. Gün Salı","3. Gün Çarşamba","4. Gün Perşembe","5. Gün Cuma","6. Gün Cumartesi","7. Gün Pazar"]
-print(gunler[0])
-print(gunler[1])
---
+++ ornek6.py
@@ -0,0 +1,21 @@
+tek_sayilar=[3,5,7,9,11,13,15,17,19]
+
+print(tek_sayilar[0:6]) #[3,5,7,9,11,13]
+
+
+print(tek_sayilar[2:5]) #[7,9,11]
+
+
+print(tek_sayilar[3:8]) #[9,11,13,15,17]
+
+
+print(tek_sayilar[:5]) #[3,5,,7,9,11]
+
+
+print(tek_sayilar[3:]) #[9,11,13,15,17,19]
+
+
+print(tek_sayilar[0:8:2]) #[3,7,11,15]
+
+
+print(tek_sayilar[::3]) #[3,9,15]
