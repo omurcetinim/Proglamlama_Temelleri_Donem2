8031a375c2e261b6218388819f1dcbba92418fb6
omurcetinim <omurcetin963@gmail.com>
2025-03-10 12:07:41 +0300

    Add files via upload

---
+++ 250310/ornek2.py
@@ -0,0 +1,6 @@
+k_ad=input("Kullanıcı adını girin:")
+sifre=input("Şifre girin:")
+if (k_ad == "Türkiye" and sifre == "1923"):
+    print("Giriş Başarılı😊")
+else:
+    print("Kullanıcı adı ya da şifre yanlış🙁")
