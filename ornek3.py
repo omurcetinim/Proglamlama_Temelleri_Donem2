518d441268acf51909b8714a2c1a81740737bc9a
omurcetinim <omurcetin963@gmail.com>
2025-04-14 12:09:04 +0300

    Add files via upload

--- ornek3.py
+++
@@ -1,7 +0,0 @@
-kullanici_adi=int(input("Adınızı giriniz: "))
-if kullanici_adi <0:
-    print("Karakter saysı boş girilemez")
-elif kullanici_adi >0:
-    print("Karakter sayısı az")
-else:
-    print("Kullanıcı adı uzun")
