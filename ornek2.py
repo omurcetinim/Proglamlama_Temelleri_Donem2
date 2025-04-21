71177edaa279010fea15b113cfb0033545c5158b
omurcetinim <omurcetin963@gmail.com>
2025-04-21 12:11:38 +0300

    Add files via upload

--- ornek2.py
+++
@@ -1,7 +0,0 @@
-park_sure=int(input("Otoparkta duracağınız dakikayı giriniz: "))
-if park_sure <=60:
-    print("Ücret=5 TL")
-elif park_sure <=300:
-    print("Ücret",(park_sure/60*4))
-else:
-    print("Ücret",(park_sure/60*3))
--- ornek5.py
+++ ornek5.py
@@ -1,2 +1,5 @@
-asal_sayilar=[2,3,5,7,11,13,17,19,23]
-print(asal_sayilar[::2])
+sayi1=int(input("Sayı Giriniz: "))
+sayi2=int(input("Sayı Giriniz: "))
+toplam=0
+for toplam in range (sayi1,sayi2):
+    print("Toplam Sonuç: ",toplam)
